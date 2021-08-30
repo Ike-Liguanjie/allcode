259d2961c2e9273269c2ffd1632881b9680d6885
Ike <ike-liguanjie@foxmail.com>
2021-08-30 11:37:57 +0800

    插入排序算法

--- sort/insertion-sort.py
+++ sort/insertion-sort.py
@@ -15,7 +15,7 @@ def insertion_sort(arr: list) -> list:
     2.从列表第二个元素开始，不断的和前一个比较，当前一个元素insert_index>-1且比insert_num大的时候，说明满足替换条件
       此时把insert_index位置的值后移一位，即arr[insert_index + 1] = arr[insert_index]，同时insert_index-1继续比较寻找
     3.当不满足替换条件的时候，说明上一步找到的insert_index就是正确的插入位置，此时arr[insert_index + 1] = insert_num，完成插入
-    4.重复index循环直到列表尾部结束，此时列表有序。
+    4.重复index循环直到列表尾部结束，此时列表有序
     :param arr:
     :return:
     """
